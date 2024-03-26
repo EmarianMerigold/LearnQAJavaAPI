7d6f0f48d95b31406362ab341e80388e9a05e0fb
Emrn <kostyan.tsk@gmail.com>
2024-03-27 00:54:01 +0700

    HelloWorldTest_#15_Получение_Cookie_и_передача_их_в_двугой_запрос

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -11,8 +11,8 @@ public class HelloWorldTest {
     @Test
     public void testRestAssured(){
         Map<String, String> data = new HashMap<>();
-        data.put("login", "secret_login2");
-        data.put("password", "secret_pass2");
+        data.put("login", "secret_login");
+        data.put("password", "secret_pass");
 
         Response response = RestAssured
                 .given()
@@ -20,6 +20,11 @@ public class HelloWorldTest {
                 .when()
                 .get("https://playground.learnqa.ru/api/get_auth_cookie")
                 .andReturn();
+
+        /*
+        System.out.println("\nCode:");
+        int statusCode = response.getStatusCode();
+        System.out.println(statusCode);
 
         System.out.println("\nPretty text:");
         response.prettyPrint();
@@ -32,11 +37,34 @@ public class HelloWorldTest {
         System.out.println("\nCookies:");
         Map<String,String> responseCookies = response.getCookies();
         System.out.println(responseCookies);
+        */
+
+        //-------------------------------------------------------------------
 
         //Получение конкретного значения у параметра auth_cookie
         System.out.println("\nCookie_key:");
         String responseCookie = response.getCookie("auth_cookie");
         System.out.println(responseCookie);
+
+        // Создание переменной, в которую будет записываться значение полученного cookie.
+        // Также сделано условие, что записываться cookie будет только валидное. Значении null не сохраняется.
+        Map<String, String> cookies = new HashMap<>();
+        if(responseCookie != null) {
+            cookies.put("auth_cookie", responseCookie);
+        }
+
+        // В данном запросе для авторизовации по cookie передаётся сохранённое значение cookie,
+        // полученное из предыдущего запроса
+        Response responceForCheck = RestAssured
+                .given()
+                .body(data)
+                .cookies(cookies)
+                .when()
+                .get("https://playground.learnqa.ru/api/check_auth_cookie")
+                .andReturn();
+
+        System.out.println("\n");
+        responceForCheck.print();
 
 
     }
