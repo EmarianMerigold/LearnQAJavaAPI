65f8dd63d07b9ac7da9fa38f242265c7e04ca25c
Emrn <kostyan.tsk@gmail.com>
2024-03-26 19:54:22 +0700

    HelloWorldTest_#12_Получение_заголовков_от_сервера

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,24 +1,35 @@
 import io.restassured.RestAssured;
+import io.restassured.http.Headers;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class HelloWorldTest {
 
     @Test
     public void testRestAssured(){
+        Map<String, String> headers = new HashMap<>();
+        headers.put("myHeader1", "myValue1");
+        headers.put("myHeader2", "myValue2");
 
         Response response = RestAssured
                 .given()
+                //.headers(headers)
                 .redirects()
                 .follow(false)
                 .when()
-                //.get("https://playground.learnqa.ru/api/check_type")
-                //.get("https://playground.learnqa.ru/api/get_500")
+                //.get("https://playground.learnqa.ru/api/show_all_headers")
                 .get("https://playground.learnqa.ru/api/get_303")
                 .andReturn();
 
-        int statusCode = response.getStatusCode();
-        System.out.println(statusCode);
+        response.prettyPrint();
+
+        //Headers responseHeaders = (Headers) response.getHeaders(); // Получение всех заголовков
+        //System.out.println(responseHeaders);
+        String locationHeader = response.getHeader("Location"); // Получение конкретного заголовка
+        System.out.println(locationHeader);
 
     }
 }
