c641d0df96ef93237f27d3cbff4eba40ad6e4a9c
Emrn <kostyan.tsk@gmail.com>
2024-03-26 22:11:37 +0700

    HelloWorldTest_#14_Получение_Cookie

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -10,26 +10,34 @@ public class HelloWorldTest {
 
     @Test
     public void testRestAssured(){
-        Map<String, String> headers = new HashMap<>();
-        headers.put("myHeader1", "myValue1");
-        headers.put("myHeader2", "myValue2");
+        Map<String, String> data = new HashMap<>();
+        data.put("login", "secret_login2");
+        data.put("password", "secret_pass2");
 
         Response response = RestAssured
                 .given()
-                //.headers(headers)
-                .redirects()
-                .follow(false)
+                .body(data)
                 .when()
-                //.get("https://playground.learnqa.ru/api/show_all_headers")
-                .get("https://playground.learnqa.ru/api/get_303")
+                .get("https://playground.learnqa.ru/api/get_auth_cookie")
                 .andReturn();
 
+        System.out.println("\nPretty text:");
         response.prettyPrint();
 
-        //Headers responseHeaders = (Headers) response.getHeaders(); // Получение всех заголовков от сервера
-        //System.out.println(responseHeaders);
-        String locationHeader = response.getHeader("Location"); // Получение конкретного заголовка от сервера
-        System.out.println(locationHeader);
+        System.out.println("\nHeaders:");
+        Headers responseHeaders = response.getHeaders();
+        System.out.println(responseHeaders);
+
+        //Получение всех cookie ключ/значение
+        System.out.println("\nCookies:");
+        Map<String,String> responseCookies = response.getCookies();
+        System.out.println(responseCookies);
+
+        //Получение конкретного значения у параметра auth_cookie
+        System.out.println("\nCookie_key:");
+        String responseCookie = response.getCookie("auth_cookie");
+        System.out.println(responseCookie);
+
 
     }
 }
