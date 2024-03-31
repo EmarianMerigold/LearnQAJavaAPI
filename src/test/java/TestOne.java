b174a0ebdc840f83eaf469d6875ce3b291d7237e
Emrn <kostyan.tsk@gmail.com>
2024-04-01 01:19:11 +0700

    TestOne_#1_200_and_404_tests

---
+++ src/test/java/TestOne.java
@@ -0,0 +1,31 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class TestOne {
+
+    @Test
+    public void testFor200() {
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/map")
+                .andReturn();
+        //assertTrue(response.statusCode() == 200, "Unexpected status code");
+        assertEquals(200, response.statusCode(), "Unexpected status code");
+
+    }
+
+    @Test
+    public void testRestFor404() {
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/map2")
+                .andReturn();
+        assertEquals(404, response.statusCode(), "Unexpected status code");
+
+    }
+
+
+}
