cf82fd20d4ecdf93ce54c2ce5dc2a6d870bbaa41
Emrn <kostyan.tsk@gmail.com>
2024-04-10 20:52:32 +0700

    фикс

--- src/test/java/oldTests/UserAuthTest.java
+++ src/test/java/oldTests/UserAuthTest.java
@@ -1,27 +1,24 @@
+package oldTests;
+
 import io.restassured.RestAssured;
-import io.restassured.http.Headers;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import io.restassured.http.Headers;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashMap;
 import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class UserAuthTestBeforeEach {
+public class UserAuthTest {
 
-    String cookie;
-    String header;
-    int userIdOnAuth;
-
-    @BeforeEach
-    public void  loginUser(){
+    @Test
+    public void testAuthUser(){
         Map<String, String> authData = new HashMap<>();
         authData.put("email", "vinkotov@example.com");
         authData.put("password", "1234");
@@ -32,18 +29,20 @@ public class UserAuthTestBeforeEach {
                 .post("https://playground.learnqa.ru/api/user/login")
                 .andReturn();
 
-        this.cookie = responseGetAuth.getCookie("auth_sid");
-        this.header = responseGetAuth.getHeader("x-csrf-token");
-        this.userIdOnAuth = responseGetAuth.jsonPath().getInt("user_id");
-    }
+        Map<String, String> cookies = responseGetAuth.getCookies();
+        Headers headers = responseGetAuth.getHeaders();
+        int userIdOnAuth = responseGetAuth.jsonPath().getInt("user_id");
 
-    @Test
-    public void testAuthUser(){
+        assertEquals(200, responseGetAuth.statusCode(), "Unexpected status code");
+        assertTrue(cookies.containsKey("auth_sid"), "Response doesn't have 'auth_sid' cookie");
+        assertTrue(headers.hasHeaderWithName("x-csrf-token"), "Response doesn't have 'x-csrf-token' cookie");
+        //assertTrue(responseGetAuth.jsonPath().getInt("user_id") > 0,  "User id should be greater than 0");
+        assertTrue(userIdOnAuth > 0,  "User id should be greater than 0");
 
         JsonPath responseCheckAuth = RestAssured
                 .given()
-                .header("x-csrf-token", this.header)
-                .cookie("auth_sid", this.cookie)
+                .header("x-csrf-token", responseGetAuth.getHeader("x-csrf-token"))
+                .cookie("auth_sid", responseGetAuth.getCookie("auth_sid"))
                 .get("https://playground.learnqa.ru/api/user/auth")
                 .jsonPath();
 
@@ -56,14 +55,26 @@ public class UserAuthTestBeforeEach {
     @ParameterizedTest
     @ValueSource(strings = {"cookie", "headers"})
     public void testNegativeAuthUser(String condition){
+        Map<String, String> authData = new HashMap<>();
+        authData.put("email", "vinkotov@example.com");
+        authData.put("password", "1234");
+
+        Response responseGetAuth = RestAssured
+                .given()
+                .body(authData)
+                .post("https://playground.learnqa.ru/api/user/login")
+                .andReturn();
+
+        Map<String, String> cookies = responseGetAuth.getCookies();
+        Headers headers = responseGetAuth.getHeaders();
 
         RequestSpecification spec = RestAssured.given();
         spec.baseUri("https://playground.learnqa.ru/api/user/auth");
 
         if (condition.equals("cookie")) {
-            spec.cookie("auth_sid", this.cookie);
+            spec.cookie("auth_sid", cookies.get("auth_sid"));
         } else if (condition.equals("headers")){
-            spec.header("x-csrf-token", this.header);
+            spec.header("x-csrf-token", headers.get("x-csrf-token"));
         } else {
             throw new IllegalArgumentException("Condition value is known: " + condition);
         }
