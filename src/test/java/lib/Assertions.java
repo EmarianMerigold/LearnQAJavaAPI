023caea1f65c16c36224f3272eec627d279a2f77
Emrn <kostyan.tsk@gmail.com>
2024-04-11 18:38:48 +0700

    - Создание нового пользователя - Получение инфы о пользователе с куки и без

--- src/test/java/lib/Assertions.java
+++ src/test/java/lib/Assertions.java
@@ -2,6 +2,7 @@ package lib;
 
 import io.restassured.response.Response;
 import static org.hamcrest.Matchers.hasKey;
+import static org.hamcrest.Matchers.not;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Assertions {
@@ -29,6 +30,21 @@ public class Assertions {
                 "Response status code is nit as expected"
         );
 
+    }
+
+    public static void assertJsonHasKey(Response Response, String expectedFieldName){
+        Response.then().assertThat().body("$", hasKey(expectedFieldName));
+    }
+
+    public static void assertJsonHasNotKey(Response Response, String unexpectedFieldName){
+        Response.then().assertThat().body("$", not(hasKey(unexpectedFieldName)));
+
+    }
+
+    public static void assertJsonHasFields(Response Response, String[] expectedFieldNames){
+        for (String expectedFieldName : expectedFieldNames){
+            Assertions.assertJsonHasKey(Response, expectedFieldName);
+        }
     }
 
 }
---
+++ src/test/java/tests/UserGetTest.java
@@ -0,0 +1,60 @@
+package tests;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import lib.Assertions;
+import lib.BaseTestCase;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UserGetTest extends BaseTestCase {
+
+    @Test
+    public  void testGetUserDataNonAuth(){
+        Response responseUserData = RestAssured
+                .get("https://playground.learnqa.ru/api/user/2")
+                .andReturn();
+
+        //System.out.println(responseUserData.asString());
+        Assertions.assertJsonHasKey(responseUserData, "username");
+        Assertions.assertJsonHasNotKey(responseUserData, "firstName");
+        Assertions.assertJsonHasNotKey(responseUserData, "lastName");
+        Assertions.assertJsonHasNotKey(responseUserData, "email");
+
+    }
+
+    @Test
+    public void testGetUserDetailsAuthAsSameUser(){
+        Map<String, String> authData = new HashMap<>();
+        authData.put("email", "vinkotov@example.com");
+        authData.put("password", "1234");
+
+        Response responseGetAuth = RestAssured
+                .given()
+                .body(authData)
+                .post("https://playground.learnqa.ru/api/user/login")
+                .andReturn();
+
+        String header = this.getHeader(responseGetAuth, "x-csrf-token");
+        String cookie = this.getCookie(responseGetAuth, "auth_sid");
+
+        Response responseUserData = RestAssured
+                .given()
+                .header("x-csrf-token", header)
+                .cookie("auth_sid", cookie)
+                .get("https://playground.learnqa.ru/api/user/2")
+                .andReturn();
+
+        String[] expectedFields = {"username", "firstName", "lastName", "email"};
+        Assertions.assertJsonHasFields(responseUserData, expectedFields);
+        /*
+        Assertions.assertJsonHasKey(responseUserData, "username");
+        Assertions.assertJsonHasKey(responseUserData, "firstName");
+        Assertions.assertJsonHasKey(responseUserData, "lastName");
+        Assertions.assertJsonHasKey(responseUserData, "email");
+         */
+    }
+
+}
