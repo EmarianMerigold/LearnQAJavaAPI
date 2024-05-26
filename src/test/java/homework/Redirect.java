c528b3cd8dc5041a0de3c7789145ac9728944e30
Emrn <kostyan.tsk@gmail.com>
2024-05-27 03:05:46 +0700

    Доработки по редиректу. Ex6

--- src/test/java/homework/Redirect.java
+++ src/test/java/homework/Redirect.java
@@ -1,6 +1,7 @@
 package homework;
 
 import io.restassured.RestAssured;
+import io.restassured.http.Headers;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
@@ -17,13 +18,18 @@ public class Redirect {
                 .get("https://playground.learnqa.ru/api/long_redirect")
                 .andReturn();
 
+        //Вывод всех заголовков (чтобы узнать название параметра(ключа), в котором хранится url для редиректа)
+        Headers responseHeaders = response.getHeaders();
+        System.out.println(responseHeaders);
+
         //Вывод кода ответа
         int statusCode = response.getStatusCode();
         System.out.println("Status code: " + statusCode);
 
-        //Вывод адреса редиректа
+        //Вывод адреса редиректа из параметра Location
         String location = response.getHeader("Location");
         System.out.println("URL for redirect: " + location);
+
 
     }
 
